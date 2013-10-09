81199249bfce2f0501fd1402ba2965a5b5f970e6
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2013-10-08 23:36:32 -0400

    Working on adding way to visualize existing Ruby classes

--- create_graph.sh
+++ create_graph.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-grep -r 'lib' -e 'class' > files.txt
+rm -f force.csv
+grep -r '../lib' -e 'class' > files.txt
 cut -d ' ' -f2- files.txt > classes.txt
 echo 'source,target' > force.csv
 
@@ -14,7 +15,9 @@ do
 		do
 			out="$w $out"
 		done
-	echo $out | tr -d ' ' | tr '<' ',' >> force.csv
+		echo $out | tr -d ' ' | tr '<' ',' >> force.csv
+		cut -f1 -d ' ' $out
+	echo "Ruby,$temp" #>> force.csv
 	else
 		echo "Ruby,$line" >> force.csv
 	fi
