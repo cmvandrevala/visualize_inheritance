e5056108486289f0fe4da53acb8ff0d0663b7a2a
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2013-10-09 12:02:42 -0400

    Working directed graph builder

---
+++ README.md
@@ -0,0 +1,6 @@
+Visualizing Inheritance Structure in Ruby
+=========================================
+
+This code uses a simple Bash script combined with a force directed graph in D3 to visualize the inheritance structure of a Ruby project.
+
+Put this in the main folder of the project.  Upon running the bash script create_graph.sh, it will scan the lib folder for all classes, and then create a force directed graph in a browser window using D3.
--- create_graph.sh
+++ create_graph.sh
@@ -1,28 +1,45 @@
 #!/bin/sh
 
-rm -f force.csv
-grep -r '../lib' -e 'class' > files.txt
-cut -d ' ' -f2- files.txt > classes.txt
-echo 'source,target' > force.csv
+#Clean Up Folder
+rm -f files.txt trimmed_files.txt classes.txt only_commas.txt spaced_classes.txt reordered_classes.txt force.csv
 
-exec < classes.txt
-while read line
-do
-	if [[ $line == *\<* ]]
-	then
-		out=""
-		for w in $line
-		do
-			out="$w $out"
+#Collect All Classes in Each File in ../lib
+grep -r '../lib' -e 'class' > files.txt
+
+#Trim All Whitespace
+tr -d ' \t\r\f' < files.txt > trimmed_files.txt
+
+#Remove Directory Information
+egrep -o "(class\S+)" trimmed_files.txt | cut -c 6- > classes.txt
+
+#Add Spaces for Future Processing
+sed 's/</ < /g' classes.txt >> spaced_classes.txt
+
+#Reverse Each Line
+awk '{ for (i=NF; i>=1; i--) printf (i!=1) ? $i OFS : $i "\n" }' spaced_classes.txt >> reordered_classes.txt
+
+#Create force.csv file
+echo 'source,target' > force.csv
+exec < reordered_classes.txt
+while read LINE; do
+	if [[ $LINE == *\<* ]]; then
+		Y=0
+		for WORD in $LINE; do
+			if [ $Y == 0 ]
+				then
+				PARENT=$WORD
+			elif [ $Y == 2 ]
+				then
+				CHILD=$WORD
+			fi
+			Y=$((Y+1))
 		done
-		echo $out | tr -d ' ' | tr '<' ',' >> force.csv
-		cut -f1 -d ' ' $out
-	echo "Ruby,$temp" #>> force.csv
+		echo "$PARENT,$CHILD" >> force.csv
+		echo "Ruby,$PARENT" >> force.csv
 	else
-		echo "Ruby,$line" >> force.csv
+		echo "Ruby,$LINE" >> force.csv
 	fi
 done
 
 open index.html
-rm files.txt
-rm classes.txt
+rm -f files.txt trimmed_files.txt classes.txt only_commas.txt spaced_classes.txt reordered_classes.txt
