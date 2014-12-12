6b61d6d36b78660799194ea990d888ec3895e4c8
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2014-12-12 16:50:27 -0500

    Fix bug visualizing objects

--- bin/visualize.sh
+++ bin/visualize.sh
@@ -3,7 +3,7 @@
 grep -r 'lib' -e 'class' > files.txt
 cut -d ' ' -f2- files.txt > classes.txt
 echo 'source,target' > force.csv
-echo 'BasicObject,Object' > force.csv
+echo 'BasicObject,Object' >> force.csv
 
 exec < classes.txt
 while read line
